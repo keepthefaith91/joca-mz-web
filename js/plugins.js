3831dbbbbede09a3cdb916eebd77ed224ca742f0
keepthefaith91 <197049062+keepthefaith91@users.noreply.github.com>
2025-02-25 20:02:04 -0800

    web-1.0.0-beta.3

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"Fomar0153_TileSize","status":true,"description":"Allows you to use tiles of a different size in game.","parameters":{"Tile Size":"32","Key String":"!"}},
 {"name":"yanflycoreengine","status":false,"description":"","parameters":{}},
-{"name":"Zoom","status":true,"description":"v0.6.0 Map zoom","parameters":{"Default zoom for maps":"1.50","Always free camera":"false","Fix sprite black lines":"true","Fix map encounter zoom":"true"}}
+{"name":"Zoom","status":true,"description":"v0.6.0 Map zoom","parameters":{"Default zoom for maps":"1.55","Always free camera":"false","Fix sprite black lines":"true","Fix map encounter zoom":"true"}},
+{"name":"Theo_EnemyHPGauge","status":true,"description":"v1.0.1 - Theo Enemy HP Gauge | Visual HP gauge for the enemies","parameters":{"option":"","pos":"Top","defeat":"false","alwaysShown":"false","prop":"","width":"100","height":"11","animeDur":"45","displayDur":"25","border":"2","segments":"1","gaugecolor":"","lcolorindex":"20","rcolorindex":"21","bcolorindex":"15","aftImage":"18","aftAlpha":"200"}}
 ];
